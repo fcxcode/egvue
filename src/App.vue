28fd8b38eddf42270eb2da423aa8308cfd2d0560
fcxcode <fcx.code@gmail.com>
2019-01-27 19:58:12 +0800

    added activities charts

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,27 @@
 <template>
   <div id="app">
+    <activities-charts title="Extreme Games"/>
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
+import ActivitiesCharts from "./components/ActivitiesCharts.vue";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    HelloWorld
+    HelloWorld,
+    ActivitiesCharts
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
---
+++ src/components/ActivitiesCharts.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <h1>{{ title }}</h1>
+    <div style="width:65%; margin: auto;">
+      <canvas id="activities-charts"></canvas>
+    </div>
+  </div>
+</template>
+
+<script>
+import Chart from "chart.js";
+import activitiesChartsData from "../chart-data.js";
+
+export default {
+  name: "ActivitiesCharts",
+  props: {
+    title: String
+  },
+  data() {
+    return {
+      activitiesChartsData: activitiesChartsData
+    };
+  },
+  mounted() {
+    this.createChart("activities-charts", this.activitiesChartsData);
+  },
+  methods: {
+    createChart(chartId, chartData) {
+      const ctx = document.getElementById(chartId);
+      new Chart(ctx, {
+        type: chartData.type,
+        data: chartData.data,
+        options: chartData.options
+      });
+    }
+  }
+};
+</script>
